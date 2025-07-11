03e9e663d72ef12ac31c34aeaf1b56e1894c8b5c
RenateSchmeidlerLMUCampus <R.Schmeidler@campus.lmu.de>
2025-07-11 20:29:29 +0200

    Add files via upload

---
+++ code/Code_Create_Graphics.R
@@ -0,0 +1,189 @@
+## Code zum Erzeugen der Graphiken zum Veranschaulichen 
+## der unterschiedlichen Güte der Laplace-Approximation 
+## bei verschiedenen Verteilungen
+
+library(ggplot2)
+library(numDeriv)
+
+## ==================================================================
+## Funktion "laplace_approximation" 
+## zum Berechnen der Laplace-Approximation für eine Funktion probfunc
+
+
+
+laplace_approximation <- function(probfunc, lowBound, uppBound){
+  
+  ### (A) Preparation ----------------------------------------------- 
+  neg_log_f <- function(x){
+    -log(probfunc(x))
+  }
+  
+  ### (B) Optimization ----------------------------------------------
+  optim <- optimize(neg_log_f, 
+                    lower = lowBound, upper = uppBound)
+  th_hat <- optim$minimum
+  
+  ### (C) Hessian ---------------------------------------------------
+  H <- hessian(neg_log_f, x = th_hat)
+  
+  ### (D) Inverse Hessian -------------------------------------------
+  inv_H <- solve(H)
+  
+  ### (E) Result ----------------------------------------------------
+  f <- function(x){
+    dnorm(x, mean = th_hat, sd = sqrt(inv_H))
+  }
+  return(f)
+}
+## ==================================================================
+
+## Funktion "compFunc_sameImage" 
+## zum Darstellen zweier gegebener Funktionen func1 und func2
+## in einer gemeinsamen Graphik
+## func1 : rot, durchgezogene Linie
+## func2 : blau, gestrichelte Linie
+
+
+compFunc_sameImage <- function(lowB, upB, func1, func2){
+  base <- 
+    ggplot() + 
+    xlim(lowB,upB)
+  g <- 
+    base +
+    geom_function(fun = func1, 
+                  color = "Red", linetype = "solid", 
+                  linewidth = 1) +
+    geom_function(fun = func2, 
+                  color = "Blue", linetype = "longdash", 
+                  linewidth = 1) +
+    theme(axis.line = 
+            element_line(
+              arrow = arrow(length = unit(0.1, "inches")), 
+            ))
+  return(g)
+}
+
+## =================================================================
+## Funktion "comp_lapAppr_sameImage" zum Darstellen 
+## einer Funktion func und ihrer Laplace-Approximation lapAppr
+## in einer gemeinsamen Graphik
+## Originalfunktion func : rot, durchgezogene Linie
+## Laplace-Approximation : blau, gestrichelte Linie
+
+comp_lapAppr_sameImage <- function(func, lowB, upB){
+  lapAppr <- laplace_approximation(func, lowB, upB)
+  g <- compFunc_sameImage(lowB, upB, func, lapAppr )
+  return(g)
+}
+
+## =================================================================
+## =================================================================
+
+## Beispiele mit Normalverteilung
+
+test_Norm <- function(m0, sd0){
+  f <- function(x){
+    dnorm(x, mean = m0, sd = sd0)
+  }
+  return(f)
+}
+
+gNorm_0_1   <- comp_lapAppr_sameImage(test_Norm(0,1), -4, 4)
+gNorm_25_7  <- comp_lapAppr_sameImage(test_Norm(25,7), 20, 29)
+
+## -----------------------------------------------------------------
+## Beispiele mit t-Verteilung
+
+test_tdistr <- function(df0, ncp0){
+  f <- function(x){
+    dt(x, df = df0, ncp = ncp0)
+  }
+  return(f)
+}
+
+gtDistr_2_0   <- comp_lapAppr_sameImage(test_tdistr(2, 0), -4, 4)
+gtDistr_20_0  <- comp_lapAppr_sameImage(test_tdistr(20, 0), -4, 4)
+gtDistr_200_0 <- comp_lapAppr_sameImage(test_tdistr(200, 0), -4, 4)
+gtDistr_2_5   <- comp_lapAppr_sameImage(test_tdistr(2, 5), -2, 10)
+
+
+## -----------------------------------------------------------------
+## Beispiele mit Gamma-Verteilung
+
+test_Gamma <- function(shape_selected, rate_selected){
+  f <- function(x){
+    dgamma(x, shape_selected, rate_selected)
+  }
+  return(f)
+}
+## Bemerkung: Damit die Laplace-Approximation funktioniert, 
+## muss shape_selected > 1 sein
+
+gGamma_1.1_1   <- comp_lapAppr_sameImage(test_Gamma(1.1, 1), 0, 4)
+gGamma_1.5_1   <- comp_lapAppr_sameImage(test_Gamma(1.5, 1), 0, 4)
+gGamma_2_1     <- comp_lapAppr_sameImage(test_Gamma(2, 1), 0, 4)
+gGamma_20_1    <- comp_lapAppr_sameImage(test_Gamma(20, 1), 0, 40)
+gGamma_20_10   <- comp_lapAppr_sameImage(test_Gamma(20, 10), 0, 4)
+
+
+## -----------------------------------------------------------------
+## Beispiele mit Beta-Verteilung
+
+test_Beta <- function(a_selected, b_selected){
+  f <- function(x){
+    dbeta(x, a_selected, b_selected)
+  }
+  return(f)
+}
+
+gBeta_2_2      <- comp_lapAppr_sameImage(test_Beta(2,2), 0, 1)
+gBeta_3_3      <- comp_lapAppr_sameImage(test_Beta(3,3), 0, 1)
+gBeta_20_20    <- comp_lapAppr_sameImage(test_Beta(20,20), 0, 1)
+gBeta_200_200  <- comp_lapAppr_sameImage(test_Beta(200,200), 0, 1)
+
+gBeta_2_4      <- comp_lapAppr_sameImage(test_Beta(2,4), 0, 1)
+gBeta_4_2      <- comp_lapAppr_sameImage(test_Beta(4,2), 0, 1)
+gBeta_40_20    <- comp_lapAppr_sameImage(test_Beta(40,20), 0, 1)
+gBeta_400_200  <- comp_lapAppr_sameImage(test_Beta(400,200), 0, 1)
+
+gBeta_2_40     <- comp_lapAppr_sameImage(test_Beta(2,40), 0, 1)
+gBeta_2_20     <- comp_lapAppr_sameImage(test_Beta(2,20), 0, 1)
+gBeta_2_200    <- comp_lapAppr_sameImage(test_Beta(2,200), 0, 1)
+
+gBeta_20_200   <- comp_lapAppr_sameImage(test_Beta(20,200), 0, 1)
+
+## -----------------------------------------------------------------
+
+## Beispiele mit multimodaler Verteilung
+
+test_BiModNorm <- function(m1, sd1, m2, sd2){
+  f <- function(x){
+    0.5 * (dnorm(x, mean = m1, sd = sd1) + dnorm(x, mean = m2, sd = sd2))
+  }
+  return(f)
+}
+
+test_MultiModNorm4 <- function(m1, sd1, m2, sd2, m3, sd3, m4, sd4){
+  f <- function(x){
+    0.25 * (dnorm(x, mean = m1, sd = sd1) + 
+              dnorm(x, mean = m2, sd = sd2) +
+              dnorm(x, mean = m3, sd = sd3) + 
+              dnorm(x, mean = m4, sd = sd4))
+  }
+  return(f)
+}
+
+gBiModNorm_0_1_5_1     <- comp_lapAppr_sameImage(test_BiModNorm(0, 1, 5, 1), -5, 7 )
+gBiModNorm_0_2_5_1     <- comp_lapAppr_sameImage(test_BiModNorm(0, 2, 5, 1), -5, 7)
+gBiModNorm_n5_1_0_2    <- comp_lapAppr_sameImage(test_BiModNorm(-5, 1, 0, 2),-7, 4)
+gBiModNorm_n4_0k5_0_2  <- comp_lapAppr_sameImage(test_BiModNorm(-4, 0.5, 0, 2),-7, 4)
+
+gMultiModNorm4_0_1_4_0k5_5_0k2_7_1k5<- 
+  comp_lapAppr_sameImage(test_MultiModNorm4(0, 1, 4, 0.5, 5, 0.2, 7, 1.5), -2, 10 )
+gMultiModNorm4_0_1_4_0k5_5_0k2_7_0k4 <- 
+  comp_lapAppr_sameImage(test_MultiModNorm4(0, 1, 4, 0.5, 5, 0.2, 7, 0.4), -2, 10 )
+gMultiModNorm4_0_1_4_0k5_5_0k2_7_1 <- 
+  comp_lapAppr_sameImage(test_MultiModNorm4(0, 1, 4, 0.5, 5, 0.2, 7, 1), -2, 10 )
+gMultiModNorm4_0_1_4_0k5_5_0k2_7_2 <- 
+  comp_lapAppr_sameImage(test_MultiModNorm4(0, 1, 4, 0.5, 5, 0.2, 7, 2), -2, 10 )
+
--- code/Code_Save_Graphics.R
+++ code/Code_Save_Graphics.R
@@ -24,12 +24,31 @@ ggsave(filename = "TestGamma_20_10.png",  plot = gGamma_20_10)
 ## Beispiele mit Beta-Verteilung
 
 ggsave(filename = "TestBeta_2_2.png",     plot = gBeta_2_2)
+ggsave(filename = "TestBeta_3_3.png",     plot = gBeta_3_3)
 ggsave(filename = "TestBeta_20_20.png",   plot = gBeta_20_20)
 ggsave(filename = "TestBeta_200_200.png", plot = gBeta_200_200)
 
 ggsave(filename = "TestBeta_2_4.png",     plot = gBeta_2_4)
 ggsave(filename = "TestBeta_4_2.png",     plot = gBeta_4_2)
+ggsave(filename = "TestBeta_40_20.png",   plot = gBeta_40_20)
+ggsave(filename = "TestBeta_400_200.png", plot = gBeta_400_200)
 
 ggsave(filename = "TestBeta_2_20.png",    plot = gBeta_2_20)
 ggsave(filename = "TestBeta_2_200.png",   plot = gBeta_2_200)
 ggsave(filename = "TestBeta_20_200.png",  plot = gBeta_20_200)
+
+## Beispiele mit multimodaler Verteilung
+
+ggsave(filename = "TestBiModN_0_1_5_1.png",    plot = gBiModNorm_0_1_5_1)
+ggsave(filename = "TestBiModN_0_2_5_1.png",    plot = gBiModNorm_0_2_5_1)
+ggsave(filename = "TestBiModN_n4_0k5_0_2.png", plot = gBiModNorm_n4_0k5_0_2)
+ggsave(filename = "TestBiModN_n5_1_0_2.png",   plot = gBiModNorm_n5_1_0_2)
+
+ggsave(filename = "TestMultiModN4_0_1_4_0k5_5_0k2_7_0k4.png", 
+       plot     =  gMultiModNorm4_0_1_4_0k5_5_0k2_7_0k4)
+ggsave(filename = "TestMultiModN4_0_1_4_0k5_5_0k2_7_1.png", 
+       plot     =  gMultiModNorm4_0_1_4_0k5_5_0k2_7_1)
+ggsave(filename = "TestMultiModN4_0_1_4_0k5_5_0k2_7_1k5.png", 
+       plot     =  gMultiModNorm4_0_1_4_0k5_5_0k2_7_1k5)
+ggsave(filename = "TestMultiModN4_0_1_4_0k5_5_0k2_7_2.png", 
+      plot     =   gMultiModNorm4_0_1_4_0k5_5_0k2_7_2)
